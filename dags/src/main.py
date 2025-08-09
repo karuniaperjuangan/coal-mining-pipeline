7abfc5de34c6b026567d9d3ef5e5f83ef9a45ddc
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2025-08-10 00:28:59 +0700

    feat: add a file to test the functions

--- dags/src/main.py
+++ dags/src/main.py
@@ -2,9 +2,14 @@ import extractor.fetch_csv
 import extractor.fetch_mysql
 import extractor.fetch_weather_api
 import extractor.fetch_csv
-
+import forecaster.predict_production
 import dotenv
-dotenv.load_dotenv()
-extractor.fetch_mysql.fetch_mysql()
-extractor.fetch_weather_api.fetch_weather_api("2024-07-01","2025-06-30")
-extractor.fetch_csv.fetch_csv()
+
+if __name__ == "__main__":
+    # only used to test the function, actually run through dags
+    dotenv.load_dotenv()
+    extractor.fetch_mysql.fetch_mysql()
+    extractor.fetch_weather_api.fetch_weather_api("2024-07-01","2025-06-30")
+    extractor.fetch_csv.fetch_csv()
+    forecaster.predict_production.forecast_production()
+
